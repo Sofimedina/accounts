ed96a96bf16425409f23ebbf0ebbf59f26199198
Karina Sofia Medina <kamedina@expediagroup.com>
2024-01-02 13:35:30 -0300

    changes for create account
    Signed-off-by: Karina Sofia Medina <kamedina@expediagroup.com>

---
+++ src/main/java/com/skm/accounts/service/Impl/AccountsServiceImpl.java
@@ -0,0 +1,59 @@
+package com.skm.accounts.service.Impl;
+
+import com.skm.accounts.constants.AccountsConstants;
+import com.skm.accounts.dto.CustomerDto;
+import com.skm.accounts.entity.Accounts;
+import com.skm.accounts.entity.Customer;
+import com.skm.accounts.exception.CustomerAlreadyExistException;
+import com.skm.accounts.mapper.CustomerMapper;
+import com.skm.accounts.repository.AccountsRepository;
+import com.skm.accounts.repository.CustomerRepository;
+import com.skm.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service @AllArgsConstructor
+public class AccountsServiceImpl implements IAccountsService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+
+    /**
+     * @param customerDto
+     */
+
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+        Customer customer=CustomerMapper.mapToCustomer(customerDto,new Customer());
+        Optional<Customer> optionalCustomer=customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if (optionalCustomer.isPresent()){
+            throw new CustomerAlreadyExistException("Customer already exist with given mobile number:"+
+                    customerDto.getMobileNumber());
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        Customer savedCustomer=customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+
+    /**
+     * @param customer
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer){
+        Accounts newAccount=new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccountNumber=1000000000L+new Random().nextInt(900000000);
+        newAccount.setAccountNumber(randomAccountNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedBy("Anonymous");
+        newAccount.setCreatedAt(LocalDateTime.now());
+        return newAccount;
+    }
+}
